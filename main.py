dbaaf67ba08c90345ed4bfe6f89c3696ef5db7ce
mayazigdon <149329682+mayazigdon@users.noreply.github.com>
2023-11-17 18:51:55 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import json
+import tkinter as tk
+
 ALARMS_FILE = "alarms.json"
 FILE_PATH = "C:/Users/mzigdon/Documents/logs/hdmtOScommon-3.log"
+
 with open(ALARMS_FILE,'r') as json_file:
     data = json.load(json_file)
     alarms_list = data['errors']
     alarms_list = [word.lower() for word in alarms_list]
-
 try:
     with open(FILE_PATH,'r') as file:
         file_content = file.readlines()
@@ -13,27 +15,33 @@ try:
 except FileNotFoundError:
     print("file not found")
 
+count=0
+line_number = {}
+lines_dict: dict ={}
 
-lines_list:list = []
 for line in file_content:
     line = line.lower()
     line = line.strip()
     line = line.rsplit("]",1)
-    lines_list.append(line[-1])
+    lines_dict[count]=line[-1]
+    line_number[count] = line[0]
+    count += 1
 
-#for line in lines_list:
-#    for alarm in alarms_list:
-#        if alarm in line:
-#            print(line)
+number = ''
 for alarm in alarms_list:
+    curr_line = []
     print("*********************************************key word:",alarm)
-    for line in lines_list:
-        if alarm in line:
-            print(line)
+    for key, value in lines_dict.items():
+        if alarm in value:
+            number = line_number[key].rsplit(',', 1)
+            number = number[-1]
+            if number not in curr_line:
+                print(number)
+                print(lines_dict[key])
+                curr_line.append(number)
 
 
-#for alarm in alarms_list:
-#    if alarm in file_content:
-#        print(alarm)
+
+
 
 
