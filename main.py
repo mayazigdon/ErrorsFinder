4f77c4efb3bb66048768f5faf3d8afea3f47592c
mayazigdon <149329682+mayazigdon@users.noreply.github.com>
2023-11-19 02:14:05 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,47 +1,86 @@
 import json
 import tkinter as tk
+import UI as ui
 
 ALARMS_FILE = "alarms.json"
 FILE_PATH = "C:/Users/mzigdon/Documents/logs/hdmtOScommon-3.log"
 
-with open(ALARMS_FILE,'r') as json_file:
-    data = json.load(json_file)
-    alarms_list = data['errors']
-    alarms_list = [word.lower() for word in alarms_list]
-try:
-    with open(FILE_PATH,'r') as file:
-        file_content = file.readlines()
-        file.close()
-except FileNotFoundError:
-    print("file not found")
+def loading_errors_to_search()->list:
+    with open(ALARMS_FILE,'r') as json_file:
+        data = json.load(json_file)
+        alarms_list = data['errors']
+        alarms_list = [word.lower() for word in alarms_list]
+        return alarms_list
 
-count=0
-line_number = {}
-lines_dict: dict ={}
+def loading_log()->list:
+    try:
+        with open(FILE_PATH,'r') as file:
+            file_content = file.readlines()
+            file.close()
+    except FileNotFoundError:
+        print("file not found")
+    return file_content
 
-for line in file_content:
-    line = line.lower()
-    line = line.strip()
-    line = line.rsplit("]",1)
-    lines_dict[count]=line[-1]
-    line_number[count] = line[0]
-    count += 1
+def find_errors_in_log(file_content:list, alarms_list)->str:
+    count=0
+    line_number = {}
+    lines_dict: dict ={}
 
-number = ''
-for alarm in alarms_list:
-    curr_line = []
-    print("*********************************************key word:",alarm)
-    for key, value in lines_dict.items():
-        if alarm in value:
-            number = line_number[key].rsplit(',', 1)
-            number = number[-1]
-            if number not in curr_line:
-                print(number)
-                print(lines_dict[key])
-                curr_line.append(number)
+    #parsing file for 2 dicts
+    for line in file_content:
+        line = line.lower()
+        line = line.strip()
+        line = line.rsplit("]",1)
+        lines_dict[count]=line[-1]
+        line_number[count] = line[0]
+        count += 1
+
+    #looking up for any alarms in file
+    output :str =""
+    number = ''
+    for alarm in alarms_list:
+        curr_line = []
+        output += "************************* key word:"+ alarm + "**************************** \n"
+        for key, value in lines_dict.items():
+            if alarm in value:
+                number = line_number[key].rsplit(',', 1)
+                number = number[-1]
+                if number not in curr_line:
+                    output += number
+                    output += " --------------"
+                    output += lines_dict[key]
+                    output+="\n"
+                    curr_line.append(number)
+
+    return output
+
+file_content = loading_log()
+alarms_list = loading_errors_to_search()
+output = find_errors_in_log(file_content, alarms_list)
+
+#GUI
+def show_labels():
+    label_header.pack()
+    label_main.pack()
+
+def make_main_label(data:str):
+    label = tk.Label(window,text =data, font=("Arial",12),justify="center", wraplength=700)
+    return label
+
+window = tk.Tk()
+
+window.geometry("800x900")
+window.title("ErorrsFinder")
+window.configure(bg='lightblue')
+window.iconbitmap('./assetes/logo.ico')
+button = tk.Button(window, text="Start",command=show_labels)
+
+label_header = tk.Label(window, text="errors found in log are:", font=("Arial",16))
 
 
 
+button.pack(padx=20,pady=80)
+label_main = make_label(output)
 
-
+window.mainloop()
 
